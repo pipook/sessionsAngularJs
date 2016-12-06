ab26248cad35261a7879e3eaf958fb063148ce38
Daniel E. Chico Pajaro <pipook@gmail.com>
2016-12-06 16:23:01 -0500

    Se modifican los archivos Gulpfile.js, app/index.html y package.js, se agrega el archivo app/css/main.css

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,7 +1,12 @@
 // File: Gulpfile.js
 'use strict';
 var gulp 				= require('gulp'),
-	connect 			= require('gulp-connect');
+	connect 			= require('gulp-connect'),
+	stylus 				= require('gulp-stylus'),
+	nib 				= require('nib'),
+	jshint 				= require('gulp-jshint'),
+	stylish 			= require('jshint-stylish'),
+	inject 				= require('gulp-inject');
 
 
 //Servidor web de desarrollo
@@ -13,3 +18,48 @@ gulp.task('server', function(){
 		livereload: true
 	});
 });
+
+
+//Recarga el navegador cuando hay cambios en el HTML
+gulp.task('html', function(){
+	gulp.src('./app/**/*.html')
+		.pipe(connect.reload());
+});
+
+//Preprocesa archivos stylus a CSS y recarga los cambios
+gulp.task('css', function(){
+	gulp.src('./app/css/*.styl')
+		.pipe(stylus({ use: nib() }))
+		.pipe(gulp.dest('./app/css'))
+		.pipe(connect.reload());
+});
+
+
+//Busca en las carpetas de estilos y javascript los archivos que hayamos creado
+//para inyectarlos en el index.html
+gulp.task('inject', function() {
+	var sources = gulp.src(['./app/js/**/*.js', './app/css/**/*.css'], { read: false });
+	return gulp.src('index.html', { cwd: './app'})
+			   .pipe(inject(sources, { relative: true }))
+			   .pipe(gulp.dest('./app'));
+});
+
+
+//Busca errores en el JS y nos los muestra por pantalla
+gulp.task('jshint', function() {
+	return gulp.src('./app/scripts/**/*.js')
+			   .pipe(jshint('.jshintrc'))
+			   .pipe(jshint.reporter('jshint-stylish'))
+			   .pipe(jshint.reporter('fail'));
+});
+
+
+//Vigila cambios que se produzcan en el codigo
+//y lanza las tareas relacionadas
+gulp.task('watch', function(){
+	gulp.watch(['./app/**/*.html'], ['html']);
+	gulp.watch(['./app/css/**/*.styl'], ['css', 'inject']);
+	gulp.watch(['./app/js/**/*.js', './Gulpfile.js'], ['jshint', 'inject']);
+});
+
+gulp.task('default', ['server', 'inject', 'watch']);
