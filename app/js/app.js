a45060a22902e9703c57cfa978f2465c6bea4e79
Daniel E. Chico Pajaro <pipook@gmail.com>
2016-12-09 16:37:31 -0500

    Se agregan varias dependencias, como angular, jquery y otras mas

--- app/js/app.js
+++ app/js/app.js
@@ -2,4 +2,11 @@
 (function() {
 	'use strict';
 	//console.log('hola mundo!, Daniel');
+	angular.module('inicioSesion', ['ngRoute'])
+		.config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+			$routeProvider.when('/', {
+				templateUrl: 'includes/views/site/login.tpl.html'
+			});
+			$locationProvider.html5Mode(true);
+		}]);
 })();
